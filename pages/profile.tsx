386c66bddc9cdf0b9c2691a0cc9ab8e88b857ae8
Yuriy Lunyov <lunyov.yu@gmail.com>
2023-03-19 19:49:23 +0200

    Moved profile page guard to the server side

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,7 +1,26 @@
 import ProfileView from '@/src/pages/Profile/ProfileView';
+import { GetServerSidePropsContext } from 'next';
+import { getSession } from 'next-auth/react';
 
 function Profile() {
   return <ProfileView />;
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
 }
 
 export default Profile;
--- src/pages/Profile/ProfileView.tsx
+++ src/pages/Profile/ProfileView.tsx
@@ -1,31 +1,11 @@
-import { useEffect, useState } from 'react';
-import { getSession } from 'next-auth/react';
-import classes from '@/src/pages/Profile/components/scss/ProfileView.module.scss';
 import ProfileForm from '@/src/pages/Profile/components/ProfileForm/ProfileForm';
+import classes from '@/src/pages/Profile/components/scss/ProfileView.module.scss';
 
 function ProfileView() {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    getSession().then((session) => {
-      if (!session) {
-        window.location.href = '/auth';
-      } else {
-        setIsLoading(false);
-      }
-    });
-  });
-
   return (
     <section className={classes.profile}>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <h1>Your User Profile</h1>
-          <ProfileForm />
-        </>
-      )}
+      <h1>Your User Profile</h1>
+      <ProfileForm />
     </section>
   );
 }
